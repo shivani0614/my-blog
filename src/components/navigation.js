11eb035832a3948d774cf61ae840f898ae10390f
shivani0614 <shivanialeti14@gmail.com>
2024-01-25 01:31:14 -0700

    initial nav bar setup

---
+++ src/components/navigation.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const navLinks=[
+    {
+        title:'Home',
+        path:'/'
+    },
+    {
+        title:'Blog',
+        path:'/blog'
+    },
+    {
+        title:'Contact Us',
+        path:'/contact-us'
+    },
+    {
+        title:'Login',
+        path:'/login'
+    },
+]
+
+export default function Navigation(){
+    return(
+        <nav className='site-navigation'>
+            <span> My Blog </span>
+            <ul>
+                { navLinks.map((link,index)=>(
+                    <li key={index}>
+                        {link.title}
+                    </li>
+
+                ))
+
+                }
+            </ul>
+        </nav>
+    );
+};
